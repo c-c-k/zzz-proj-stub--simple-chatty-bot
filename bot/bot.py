a646a0fdb252dc4178e549680704c86dd37ccef5
c-c-k <c-c-k@nym.hush.com>
2023-04-26 16:43:02 +0300

    stage 5 done

--- bot/bot.py
+++ bot/bot.py
@@ -13,8 +13,18 @@ def main():
     print("Now I will prove to you that I can count to any number you want.")
     for i in range(int(input()) + 1):
         print(f"{i} !")
-    print("Completed, have a nice day!")
-
+    print("Let's test your programming knowledge.")
+    print("Why do we use methods?")
+    print("1. To repeat a statement multiple times.")
+    print("2. To decompose a program into several small subroutines.")
+    print("3. To determine the execution time of a program.")
+    print("4. To interrupt the execution of a program.")
+    while True:
+        if input() == "2":
+            break
+        else:
+            print("Please, try again.")
+    print("Congratulations, have a nice day!")
 
 if __name__ == "__main__":
     main()
