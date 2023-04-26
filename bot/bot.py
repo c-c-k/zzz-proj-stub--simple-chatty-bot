d7ddae92189b90a07e601c593e8dea860ff33d0f
c-c-k <c-c-k@nym.hush.com>
2023-04-26 16:43:02 +0300

    stage 4 done

--- bot/bot.py
+++ bot/bot.py
@@ -8,7 +8,13 @@ def main():
     print("Enter remainders of dividing your age by 3, 5 and 7.")
     remainder3, remainder5, remainder7 = [int(input()) for _ in range(3)]
     fleshbag_age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
-    print(f"Your age is {fleshbag_age}; that's a good time to start programming!")
+    print(f"Your age is {fleshbag_age}; "
+          "that's a good time to start programming!")
+    print("Now I will prove to you that I can count to any number you want.")
+    for i in range(int(input()) + 1):
+        print(f"{i} !")
+    print("Completed, have a nice day!")
+
 
 if __name__ == "__main__":
     main()
