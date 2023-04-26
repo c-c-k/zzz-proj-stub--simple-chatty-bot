42ea4aae67e70147055a10d99c930476ae434a88
c-c-k <c-c-k@nym.hush.com>
2023-04-26 16:38:56 +0300

    stage 3 done

--- bot/bot.py
+++ bot/bot.py
@@ -4,7 +4,11 @@ def main():
     print("Please, remind me your name.")
     fleshbag_name = input()
     print(f"What a great name you have, {fleshbag_name}!")
-
+    print("Let me guess your age.")
+    print("Enter remainders of dividing your age by 3, 5 and 7.")
+    remainder3, remainder5, remainder7 = [int(input()) for _ in range(3)]
+    fleshbag_age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+    print(f"Your age is {fleshbag_age}; that's a good time to start programming!")
 
 if __name__ == "__main__":
     main()
