0a6d1397e78eb40292b84100e2d724c5d1043d1d
c-c-k <c-c-k@nym.hush.com>
2023-04-26 16:43:02 +0300

    stage 2 done

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,10 @@
 def main():
     print("Hello! My name is Aid.")
     print("I was created in 2020.")
+    print("Please, remind me your name.")
+    fleshbag_name = input()
+    print(f"What a great name you have, {fleshbag_name}!")
+
 
 if __name__ == "__main__":
     main()
